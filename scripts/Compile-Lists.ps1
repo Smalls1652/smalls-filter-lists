f29a25963a808c5fcbd96a63c8bae50aa04896dc
Timothy Small <smalls.online@icloud.com>
2023-08-30 12:05:30 -0400

    Update compile lists
    Removes emptyFile from the compiled branch, if it exists.

--- scripts/Compile-Lists.ps1
+++ scripts/Compile-Lists.ps1
@@ -4,6 +4,13 @@ param(
     [string]$OutPath
 )
 
+$emptyFilePath = Join-Path -Path $OutPath -ChildPath "emptyFile"
+
+if (Test-Path -Path $emptyFilePath) {
+    Write-Warning "Removing emptyFile from the 'compiled' branch."
+    Remove-Item -Path $emptyFilePath -Force
+}
+
 $scriptRoot = $PSScriptRoot
 
 $filterListBuildScriptPath = Join-Path -Path $scriptRoot -ChildPath "Invoke-FilterListBuild.ps1"
@@ -19,5 +26,5 @@ Copy-Item -Path "/tmp/smalls-filter-lists/LICENSE" -Destination $licenseOutPath 
 git config user.name "github-actions"
 git config user.email "<>"
 git add *
-git commit -m "Filter lists compiled from main"
+git commit -m "Filter lists compiled from 'main' branch"
 git push origin compiled
