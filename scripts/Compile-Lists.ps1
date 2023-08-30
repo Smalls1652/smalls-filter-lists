2f9d9abd8c25a166ec85441b65386e3890007f95
Timothy Small <smalls.online@icloud.com>
2023-08-30 11:53:34 -0400

    Update compile script

--- scripts/Compile-Lists.ps1
+++ scripts/Compile-Lists.ps1
@@ -8,7 +8,16 @@ $scriptRoot = $PSScriptRoot
 
 $filterListBuildScriptPath = Join-Path -Path $scriptRoot -ChildPath "Invoke-FilterListBuild.ps1"
 
-. $filterListBuildScriptPath -RootPath "/tmp/smalls-filter-lists" -OutPath $OutPath
+. $filterListBuildScriptPath -RootPath "/tmp/smalls-filter-lists" -OutPath $OutPath -Verbose
 
-Copy-Item -Path "/tmp/smalls-filter-lists/README.md" -Destination $OutPath -Force
-Copy-Item -Path "/tmp/smalls-filter-lists/LICENSE" -Destination $OutPath -Force
+$readmeOutPath = Join-Path -Path $OutPath -ChildPath "README.md"
+$licenseOutPath = Join-Path -Path $OutPath -ChildPath "LICENSE"
+
+Copy-Item -Path "/tmp/smalls-filter-lists/README.md" -Destination $readmeOutPath -Force
+Copy-Item -Path "/tmp/smalls-filter-lists/LICENSE" -Destination $licenseOutPath -Force
+
+git config user.name "github-actions"
+git config user.email "<>"
+git add *
+git commit -m "Filter lists compiled from main"
+git push origin compiled
